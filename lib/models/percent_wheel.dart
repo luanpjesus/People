20e98b790378089eefe3ede1022f0ecc8fba647e
˜luan.pjesus˜ <luanpjesus@gmail.com>
2023-02-28 17:02:53 -0300

    "Como_utilizar_o_Slider_junto_com_o_setState"

--- lib/models/percent_wheel.dart
+++ lib/models/percent_wheel.dart
@@ -1,41 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
-class PercentWhell extends StatelessWidget {
+class PercentWhell extends StatefulWidget {
   const PercentWhell({super.key});
 
   @override
+  State<PercentWhell> createState() => _PercentWhellState();
+}
+
+class _PercentWhellState extends State<PercentWhell> {
+  double _currentSliderValue = 20;
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          CircularPercentIndicator(
-            animation: true,
-            animationDuration: 500,
-            radius: 300,
-            lineWidth: 40,
-            percent: 0.5,
-            progressColor: Colors.deepPurple,
-            backgroundColor: Colors.deepPurple.shade100,
-            circularStrokeCap: CircularStrokeCap.round,
-            center: const Text(
-              "40%",
-              style: TextStyle(fontSize: 70),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CircularPercentIndicator(
+              animation: true,
+              animationDuration: 500,
+              radius: 200,
+              lineWidth: 40,
+              percent: (_currentSliderValue.roundToDouble()) / 100,
+              progressColor: Colors.deepPurple,
+              backgroundColor: Colors.deepPurple.shade100,
+              circularStrokeCap: CircularStrokeCap.round,
+              center: Text(
+                "${_currentSliderValue.round().toString()}%",
+                style: TextStyle(fontSize: 70),
+              ),
             ),
-          ),
-          LinearPercentIndicator(
-            animation: true,
-            animationDuration: 500,
-            lineHeight: 10,
-            percent: 0.5,
-            progressColor: Colors.deepPurple,
-            backgroundColor: Colors.deepPurple.shade200,
-            barRadius: Radius.circular(40),
-            leading: Icon(Icons.arrow_circle_right),
-            trailing: Icon(Icons.arrow_back_ios),
-          )
-        ],
+            Slider(
+                value: _currentSliderValue,
+                max: 100,
+                divisions: 10,
+                onChanged: (double value) {
+                  setState(() {
+                    _currentSliderValue = value;
+                    print("object");
+                  });
+                }),
+            LinearPercentIndicator(
+              animation: true,
+              animationDuration: 500,
+              lineHeight: 30,
+              percent: (_currentSliderValue.roundToDouble()) / 100,
+              progressColor: Colors.deepPurple,
+              backgroundColor: Colors.deepPurple.shade200,
+              barRadius: Radius.circular(40),
+              leading: Icon(Icons.arrow_circle_right),
+              trailing: Icon(Icons.arrow_back_ios),
+            )
+          ],
+        ),
       ),
     );
   }
